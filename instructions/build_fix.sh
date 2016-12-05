4dd8c656f7842cb2cdf79aee2ec112212075dcf8
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2016-12-05 03:13:13 +0300

    Add audio patch

--- instructions/build_fix.sh
+++ instructions/build_fix.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-# mkdir -p ~/android/system/out/target/product/5010D/obj/KERNEL_OBJ/usr
+mkdir -p ~/android/system/out/target/product/5010D/obj/KERNEL_OBJ/usr
+
+cd ~/android/system/frameworks/av
+git apply -v ~/android/system/device/TCL/5010D/instructions/0001-libmedia-stub-out-MTK-specific-bits-audio-working.patch
+
