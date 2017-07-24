ee95c605f3254417e67414d8fb1088db169dd7f0
Alexander Mishurov <alexander.m.mishurov@gmail.com>
2017-07-24 07:05:08 +0300

    Verbose revert

--- patches/revert.sh
+++ patches/revert.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo Revert av
 cd ../../../../frameworks/av
 rm 0001-mtk-av.patch
 rm media/libmedia/mtkaudio_stubs.cpp
 git checkout .
 cd -
 
+echo Revert system
 cd ../../../../system/core
 rm 0004-libnetutils-add-MTK-bits.patch
 rm ril_service.patch
@@ -13,6 +15,7 @@ rm libnetutils/ifc_utils.c.orig
 git checkout .
 cd -
 
+echo Revert bt
 cd ../../../../system
 rm bt.patch 
 cd netd
